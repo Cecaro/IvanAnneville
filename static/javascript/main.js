6efc2574fd99ae89f6658819af94b905de78ef79
Cecaro <rbcerovic@gmail.com>
2016-02-27 01:42:26 +0000

    Dunno why this was not in last commit

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
-  $("#voix-dropdown").hover(function(){
-    $(".dropdown-menu").css("display", "block");
-  });
-  $(".nav-dropdown").mouseleave(function(){
-    $(".dropdown-menu").css("display", "none");
+
+  $('#logo-expander').click(function(){
+    console.log("Clicked the cow");
+    $('#logo-expander').addClass('animate_expander').delay(4000).queue(function(){
+      $(this).removeClass('animate_expander').dequeue();
+      if($('.nav-item').hasClass('hidden')){
+      $('.nav-item').removeClass('hidden');
+      }
+      else{
+        $('.nav-item').addClass('hidden');
+      }
+    });
   });
 });
