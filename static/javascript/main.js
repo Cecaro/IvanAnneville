66a04975cd649ff64a05a7de26148909b54e2526
Cecaro <rbcerovic@gmail.com>
2016-03-23 00:21:10 +0000

    Looking goood

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,15 +1,3 @@
 $(document).ready(function() {
 
-  $('#logo-expander').click(function(){
-    console.log("Clicked the cow");
-    $('#logo-expander').addClass('animate_expander').delay(4000).queue(function(){
-      $(this).removeClass('animate_expander').dequeue();
-      if($('.nav-item').hasClass('hidden')){
-      $('.nav-item').removeClass('hidden');
-      }
-      else{
-        $('.nav-item').addClass('hidden');
-      }
-    });
-  });
 });
