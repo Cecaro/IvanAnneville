fb33d8e3198448ea0dc3686f687c9635f2c08c05
Cecaro <rbcerovic@gmail.com>
2016-03-31 18:42:50 +0100

    Good changes to the filter bar

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,3 +1,22 @@
-$(document).ready(function() {
+function toggle_visibility(id) {
+  //loop through all the content filters to see if one is clicked. If one is, we filter the videos to only the ones which have that tag
+  //otherwise we display all the videos.
+  //So if one is clicked, first we give all the videos the hidden class, then remove it from the ones with the tag
+  if($('a#'+id).hasClass("clicked")){
+    $('span[id^="' + id + '"]').remove();
+    $('a#'+id).removeClass("clicked");
+    $('.'+id).removeClass("fade-out");
+  }
+  else {
+    $('a#'+id).append('<span class="glyphicon glyphicon-remove" id="'+ id + '"></span>');
+    $('a#'+id).addClass("clicked");
+    $('.'+id).addClass("fade-out");
+  }
+  $('#'+id).preventDefault();
+}
 
+$(".filter-btn").click(function(e) {
+    e.preventDefault();
+    $(this).find('span').toggleClass('glyphicon-chevron-right glyphicon-chevron-down');
+    $(".filters").toggleClass('hidden ')
 });
